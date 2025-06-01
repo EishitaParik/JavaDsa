028a12cbafb64e48f92b1a0cd942143040b20769
EishitaParik <parikeishita@gmail.com>
2025-06-01 16:16:06 +0530

    sum of digits

---
+++ plusminus.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String findOperation(int a, int b, int c) {
+        if(a+b ==c) {
+            return "+";
+        }
+        else
+        {
+            return "-";
+        }
+    }
+}
